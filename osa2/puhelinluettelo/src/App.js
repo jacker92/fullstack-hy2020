c98702c495ee4fb0e088b96d0b70eea866ac63e2
jacker92 <lahtinen.jaakko@gmail.com>
2021-01-02 18:06:40 +0200

    Added task 2.6

---
+++ osa2/puhelinluettelo/src/App.js
@@ -0,0 +1,61 @@
+import React, { useState } from 'react'
+
+const App = () => {
+    const [persons, setPersons] = useState([
+        { name: 'Arto Hellas' }
+    ])
+    const [newName, setNewName] = useState('')
+
+    const addName = (e) => {
+        e.preventDefault();
+        setPersons(persons.concat(
+            {
+                name: newName
+            }));
+    }
+
+    const handleNameChange = (e) => {
+        setNewName(e.target.value);
+    }
+
+    return (
+        <div>
+            <PhonebookForm addName={addName} handleNameChange={handleNameChange} />
+            <PersonDisplay persons={persons} />
+        </div>
+    )
+
+}
+
+const PhonebookForm = ({ addName, handleNameChange }) => {
+    return (
+        <>
+            <h2>Phonebook</h2>
+            <form onSubmit={addName}>
+                <div>
+                    name: <input onChange={handleNameChange} />
+                </div>
+                <div>
+                    <button type="submit">add</button>
+                </div>
+            </form>
+        </>
+    )
+}
+
+const PersonDisplay = ({ persons }) => {
+    return (
+        <div>
+            <h2>Numbers</h2>
+            {persons.map(x => <Person key={x.name} name={x.name} />)}
+        </div>
+
+    )
+}
+const Person = ({ name }) => {
+    return (
+        <p>{name}</p>
+    )
+}
+
+export default App
