17afb0ac749d6d04b6f76ffc422b879560dfc55e
jacker92 <lahtinen.jaakko@gmail.com>
2021-01-03 21:00:22 +0200

    Added task 4.4

--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -3,6 +3,13 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    return blogs.reduce((sum, next) => {
+        return sum + next.likes
+    }, 0)
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
