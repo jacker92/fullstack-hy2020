7ed4ee3c58ebd950544bdfb96426008b18b837ac
jacker92 <lahtinen.jaakko@gmail.com>
2021-01-04 09:23:11 +0200

    Extracted app to app.js

---
+++ osa4/app.js
@@ -0,0 +1,20 @@
+const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+
+app.use(cors())
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app;
