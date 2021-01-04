1fe4b52e452dfb2d247a780f5dc2f2deca2c31f1
jacker92 <lahtinen.jaakko@gmail.com>
2021-01-04 16:12:13 +0200

    Added task 4.17

--- osa4/controllers/blogs.js
+++ osa4/controllers/blogs.js
@@ -1,9 +1,12 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 require('express-async-errors')
 
 blogsRouter.get('/', async (request, response) => {
-    const blogs = await Blog.find({})
+    const blogs = await Blog
+        .find({})
+        .populate('user', { username: 1, name: 1 })
     response.json(blogs.map(blog => blog.toJSON()))
 })
 
@@ -37,8 +40,16 @@ blogsRouter.put('/:id', async (request, response) => {
 
 blogsRouter.post('/', async (request, response) => {
     const blog = new Blog(request.body)
+
+    const users = await User.find({})
+    const user = users[0]
+
+    blog.user = user.id
     const result = await blog.save()
-    response.status(201).json(result)
+
+    user.blogs = user.blogs.concat(result._id)
+    await user.save()
+    response.status(201).json(result.toJSON())
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
--- osa4/models/user.js
+++ osa4/models/user.js
@@ -10,7 +10,13 @@ const userSchema = mongoose.Schema({
     },
     name: String,
 
-    passwordHash: String
+    passwordHash: String,
+    blogs: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Blog'
+        }
+    ]
 })
 
 userSchema.set('toJSON', {
