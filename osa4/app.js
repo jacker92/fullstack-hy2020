0079339d9cf8512aeac78dc3106a5c4bb5da4964
jacker92 <lahtinen.jaakko@gmail.com>
2021-01-04 15:09:09 +0200

    Added task 4.15

--- osa4/app.js
+++ osa4/app.js
@@ -1,4 +1,5 @@
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 const middleware = require('./utils/middleware')
 const express = require('express')
 const app = express()
@@ -11,6 +12,8 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology
 app.use(cors())
 app.use(express.json())
 app.use(middleware.requestLogger)
+
+app.use('/api/users', usersRouter)
 app.use('/api/blogs', blogsRouter)
 
 app.use(middleware.unknownEndpoint)
