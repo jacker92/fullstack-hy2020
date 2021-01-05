66df28e1a97a4b90987c5932381500291abe0dec
jacker92 <lahtinen.jaakko@gmail.com>
2021-01-05 12:19:06 +0200

    Fixed bug in blog model

--- osa4/models/blog.js
+++ osa4/models/blog.js
@@ -25,7 +25,6 @@ blogSchema.set('toJSON', {
         returnedObject.id = returnedObject._id.toString()
         delete returnedObject._id
         delete returnedObject.__v
-        returnedObject.user = returnedObject.user.toString()
     }
 })
 
