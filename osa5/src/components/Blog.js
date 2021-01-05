a68bb8984db47c2192553aec1db16896b6f19f91
jacker92 <lahtinen.jaakko@gmail.com>
2021-01-05 10:28:29 +0200

    Added task 5.7

--- osa5/src/components/Blog.js
+++ osa5/src/components/Blog.js
@@ -1,8 +1,52 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  function onMouseEnterParagraph(e) {
+    e.target.style.color = '#4e5951';
+  }
+
+  function onMouseLeaveParagraph(e) {
+    e.target.style.color = 'black';
+  }
+
+  const setLike = () => {
+    console.log("setting like")
+  }
+
+  if (visible) {
+    return (
+      <div style={blogStyle}>
+        <p onMouseEnter={onMouseEnterParagraph}
+          onMouseLeave={onMouseLeaveParagraph}
+          onClick={() => setVisible(!visible)}>
+          {blog.title}
+        </p>
+        {blog.url}<br />
+          likes {blog.likes} <button onClick={() => setLike()}>Like</button><br />
+        {blog.author}
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <p
+        onMouseEnter={onMouseEnterParagraph}
+        onMouseLeave={onMouseLeaveParagraph}
+        onClick={() => setVisible(!visible)}>
+        {blog.title} {blog.author}
+      </p>
+    </div>
+  )
+}
 
 export default Blog
--- osa5/src/components/DisplayForm.js
+++ osa5/src/components/DisplayForm.js
@@ -5,7 +5,9 @@ const DisplayForm = ({ user, blogs, logout }) => {
     return (
         <div><p><b>{user.username}</b> has logged in</p>
             <button onClick={logout}>Logout</button>
-            {blogs.map(blog => <Blog key={blog.id} blog={blog} />)}
+            {blogs.map(blog =>
+                <Blog key={blog.id} blog={blog} />
+            )}
         </div>
     )
 }
