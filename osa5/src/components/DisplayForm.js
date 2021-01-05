a662b926cd90c190efd5b911acd3aa11fa719d80
jacker92 <lahtinen.jaakko@gmail.com>
2021-01-05 11:58:35 +0200

    Added task 5.9

--- osa5/src/components/DisplayForm.js
+++ osa5/src/components/DisplayForm.js
@@ -3,12 +3,23 @@ import Blog from './Blog'
 
 const DisplayForm = ({ user, blogs, logout, setLike }) => {
 
+    const compareOnLikes = (x, y) => {
+        if (x.likes < y.likes) {
+            return 1;
+        }
+        if (x.likes > y.likes) {
+            return -1;
+        }
+        return 0;
+    }
     return (
         <div><p><b>{user.username}</b> has logged in</p>
             <button onClick={logout}>Logout</button>
-            {blogs.map(blog =>
-                <Blog key={blog.id} blog={blog} setLike={setLike} />
-            )}
+            {blogs
+                .sort(compareOnLikes)
+                .map(blog =>
+                    <Blog key={blog.id} blog={blog} setLike={setLike} />
+                )}
         </div>
     )
 }
