93d7e34ad9f0ceaaa9d57c11522e934b2dafdae5
jacker92 <lahtinen.jaakko@gmail.com>
2021-01-06 17:28:40 +0200

    Added task 7.2

---
+++ osa7/routed-anecdotes/src/components/Anecdote.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Anecdote = ({ anecdote }) => {
+    return (
+        <li key={anecdote.id}>
+            {anecdote.content}
+        </li>
+    )
+}
+
+export default Anecdote
