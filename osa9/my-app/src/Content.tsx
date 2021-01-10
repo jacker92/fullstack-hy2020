8c7906044687e18657788ac9fe238c9d0bc4991d
jacker92 <lahtinen.jaakko@gmail.com>
2021-01-10 14:33:22 +0200

    Added task 9.15

--- osa9/my-app/src/Content.tsx
+++ osa9/my-app/src/Content.tsx
@@ -1,14 +1,55 @@
 import React from "react";
-import { ContentProps } from './types';
+import { ContentProps, PartProps } from './types';
+import { assertNever } from "./utils";
 
 export const Content: React.FC<ContentProps> = (props) => {
     return (
         <div>
             {props.courseParts.map(course => (
-                <p key={course.name}>
-                    {course.name} {course.exerciseCount}
-                </p>
+                <Part key={course.name} course={course} />
             ))}
         </div>
     );
 };
+
+export const Part: React.FC<PartProps> = ({ course }) => {
+
+    switch (course.name) {
+        case "Fundamentals":
+            return (
+                <div>
+                    <p key={course.name}>
+                        {course.name} {course.description} {course.exerciseCount}
+                    </p>
+                </div>
+            )
+        case "Deeper type usage":
+            return (
+                <div>
+                    <p key={course.name}>
+                        {course.name} {course.description} {course.exerciseSubmissionLink}  {course.exerciseCount}
+                    </p>
+                </div>
+            )
+        case "Using props to pass data":
+            return (
+                <div>
+                    <p key={course.name}>
+                        {course.name} {course.groupProjectCount} {course.exerciseCount}
+                    </p>
+                </div>
+            )
+        case "My Own Course Part":
+            return (
+                <div>
+                    <p key={course.name}>
+                        {course.name} {course.foo} {course.exerciseCount}
+                    </p>
+                </div>
+            )
+        default:
+            assertNever(course)
+            break;
+    }
+    return (<div></div>)
+}
--- osa9/my-app/src/types.ts
+++ osa9/my-app/src/types.ts
@@ -10,7 +10,37 @@ export interface TotalProps {
     sum: number
 }
 
-export interface CoursePart {
-    name: string,
-    exerciseCount: number
+export interface PartProps {
+    course: CoursePart
 }
+
+interface CoursePartBase {
+    name: string;
+    exerciseCount: number;
+}
+
+interface DescriptiveCoursePart extends CoursePartBase {
+    description: string;
+}
+
+interface MyOwnCoursePart extends DescriptiveCoursePart {
+    name: "My Own Course Part",
+    foo: string;
+}
+
+interface CoursePartOne extends DescriptiveCoursePart {
+    name: "Fundamentals";
+    exerciseCount: 10,
+}
+
+interface CoursePartTwo extends CoursePartBase {
+    name: "Using props to pass data";
+    groupProjectCount: number;
+}
+
+interface CoursePartThree extends DescriptiveCoursePart {
+    name: "Deeper type usage";
+    exerciseSubmissionLink: string;
+}
+
+export type CoursePart = CoursePartOne | CoursePartTwo | CoursePartThree | MyOwnCoursePart;
